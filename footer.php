e6f131f34b1e2348f3dedae09c4cc5fba1a91c89
YakkieYip <yip.jacqueline@gmail.com>
2015-11-10 22:29:55 -0500

    fixed responsive styles and header image

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 
 <footer>
+	<div class="socialFooter">
+	  <a href="https://twitter.com/YakkieYip" alt="Twitter Logo" class="social-icon"><i class="fa fa-twitter fa-2x"></i></a>
+	  <a href="https://github.com/YakkieYip"><i class="fa fa-github fa-2x"></i></a>
+	  <a href="https://www.linkedin.com/in/jacquelineyip"><i class="fa fa-linkedin fa-2x"></i></a>
+	</div>
   <div class="container">
     <p>Developed with love. &copy; Jackie Yip <?php echo date('Y'); ?></p>
   </div>
@@ -10,12 +15,13 @@
 <!-- SCRIPTS -->
 <script>
 /* Google Analytics! */
- var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
+ var _gaq=[["_setAccount","UA-69911835-1"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
  g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
  s.parentNode.insertBefore(g,s)}(document,"script"));
 </script>
 
+	
 <?php wp_footer(); ?>
 </body>
 </html>
--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,12 @@
 //load in jQuery plugin dependencies (eg. flexslider, smoothScroll etc.) in this file
 jQuery(document).ready(function(){
    jQuery('a[href^="#"]').click(function(e) {
-       jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
+   	 if (window.innerWidth <= 480) {
+   	 	jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - 40}, 1000);
+   	 }
+       else {
+   	 	jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
+       }
        return false;
        e.preventDefault();
    });
